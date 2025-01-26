40757f2becb24a94045c762f1ea6bcbcc5dc8a5c
laminar <laminar@kakao.com>
2025-01-26 23:26:54 +0900

    nojekyll

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: '200.html'
+			fallback: '404.html'
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
